7bb214ca6d2dff11296ce17f18de5e865f981032
Apurv Kumar <sapurv.skumar250@gmail.com>
2026-05-18 23:34:30 +0530

    refactor: delete unecessary items

--- globe-fly/src/main.js
+++ globe-fly/src/main.js
@@ -1,60 +1,232 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+// add axes helper
+const axesHeper = new THREE.AxesHelper(5);
 
-<div class="ticks"></div>
+//add grid helper
+const gridHelper = new THREE.GridHelper(100, 100);
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+//create a scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87ceeb);
+// scene.fog = new THREE.Fog(
+//     0x87ceeb,
+//     20,
+//     60
+// );
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+scene.add(gridHelper);
+scene.add(axesHeper);
 
-setupCounter(document.querySelector('#counter'))
+//create a camera
+const camera = new THREE.PerspectiveCamera(75,  window.innerWidth/window.innerHeight, 0.1, 4000);
+camera.position.z = 2;
+camera.position.y = 2;
+camera.position.x = 2;
+camera.lookAt(0,0,0);
+
+
+//add light
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+const sun = new THREE.DirectionalLight(0xffffff, 1);
+sun.shadow.mapSize.width = 2048;
+sun.shadow.mapSize.height = 2048;
+sun.position.set(5, 10, 7.5);
+sun.castShadow = true;
+scene.add(sun);
+
+//create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+// tree trunk
+const treeTrunk = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 3, 1),
+    new THREE.MeshStandardMaterial({
+        color: 0x8b4513
+    })
+);
+treeTrunk.position.set(5, 1.5, -5);
+treeTrunk.castShadow = true;
+scene.add(treeTrunk);
+
+// tree leaves
+const treeLeaves = new THREE.Mesh(
+    new THREE.SphereGeometry(2, 16, 16),
+    new THREE.MeshStandardMaterial({
+        color: 0x228b22
+    })
+);
+treeLeaves.position.set(5, 4, -5);
+treeLeaves.castShadow = true;
+scene.add(treeLeaves);
+
+//rocks
+for (let i = 0; i < 20; i++) {
+
+    const rock = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({
+            color: 0x777777
+        })
+    );
+
+    rock.position.set(
+        Math.random() * 50 - 25,
+        0.5,
+        Math.random() * 50 - 25
+    );
+
+    rock.castShadow = true;
+
+    scene.add(rock);
+}
+
+// cubes
+const geometry = new THREE.BoxGeometry(0.5, 1.8, 0.5);
+const material = new THREE.MeshStandardMaterial({color: 0xffaaff });
+const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
+cube.position.set(0,0,0);
+cube.position.x = THREE.MathUtils.clamp(
+    cube.position.x,
+    -10,
+    10
+);
+
+cube.position.z = THREE.MathUtils.clamp(
+    cube.position.z,
+    -10,
+    10
+);
+scene.add(cube);
+
+// ground
+const groundGeometry = new THREE.PlaneGeometry(2048, 2048);
+const groundMaterial = new THREE.MeshStandardMaterial({color: 0x3a7a3a});
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+ground.receiveShadow = true;
+scene.add(ground);
+
+//clock 
+const clock = new THREE.Clock();
+
+// controls
+const keys = {};
+const control = new OrbitControls( camera, renderer.domElement);
+control.enableDamping = true;
+
+// state controls
+  const velocity = new THREE.Vector3();
+  const cameraOffset = new THREE.Vector3(0, 3, 6);
+  const gravity = -9.8;
+  const jumpStrength = 5;
+
+  let verticalVelocity = 0;
+  let isGrounded = true;
+  let canJump = true;
+  let isFPV = false;
+
+// animate
+function animate(){
+
+  requestAnimationFrame(animate);
+  
+  const deltaTime = clock.getDelta();
+  
+  const direction = new THREE.Vector3();
+
+  const acceleration = keys["ShiftLeft"] ? 45 : 20;
+
+  const damping = 5;
+
+  if(keys['KeyP']){  
+    if(isFPV){
+      isFPV = false;
+    }
+    else{
+      isFPV = true;
+    }
+  }
+
+  if(keys["Space"] && isGrounded && canJump){
+    verticalVelocity = jumpStrength;
+    isGrounded = false;
+    canJump = false;
+  }
+
+  if(!keys["Space"]){
+    canJump = true;
+  }
+
+  verticalVelocity += gravity * deltaTime; // gravity
+  cube.position.y += verticalVelocity * deltaTime;
+
+  if(cube.position.y <= 0.9){
+    cube.position.y = 0.9;
+    verticalVelocity = 0;
+    isGrounded = true;
+  }
+
+  if (keys["KeyW"]) direction.z -= 1;
+  if (keys["KeyS"]) direction.z += 1;
+  if (keys["KeyA"]) direction.x -= 1;
+  if (keys["KeyD"]) direction.x += 1;
+
+
+  if(direction.length() > 0){
+    
+    direction.normalize();
+    
+    velocity.x += direction.x * acceleration * deltaTime;
+    velocity.z += direction.z * acceleration * deltaTime;
+    
+    cube.rotation.y += (Math.atan2(direction.x, direction.z) - cube.rotation.y) * 10 * deltaTime;
+
+  }
+  
+  //damping
+  velocity.x -= velocity.x * damping * deltaTime;
+  velocity.z -= velocity.z * damping * deltaTime;
+
+  //update position
+  cube.position.x += velocity.x * deltaTime;
+  cube.position.z += velocity.z * deltaTime;
+
+  //update camera
+  //const targetCameraPosition = cube.position.clone().add(cameraOffset);
+  //camera.position.lerp(targetCameraPosition, 5 * deltaTime);
+  control.target.copy(cube.position);
+  control.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
+// movement
+
+// event listeners
+window.addEventListener('keydown', (event) => {
+  keys[event.code] = true;
+});
+
+window.addEventListener('keyup', (event) => {
+  keys[event.code] = false;
+});
+
+// resize
+window.addEventListener( 'resize', () =>{
+  console.log('resizing');
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
